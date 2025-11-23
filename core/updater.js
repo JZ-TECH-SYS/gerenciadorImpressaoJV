64a7a9c6b7a3f1e7e30fa9892a4f543cee2a1010
joaosn <jv.zyzz.legado@gmail.com>
2025-11-23 14:11:01 -0300

    feat: adicionar suporte a atualizações automáticas e melhorar feedback ao usuário

--- core/updater.js
+++ core/updater.js
@@ -1,24 +1,53 @@
 const { info, warn, error } = require('./utils/logger');
 
-function attachAutoUpdaterHandlers(autoUpdater) {
+function attachAutoUpdaterHandlers(autoUpdater, callbacks = {}) {
+  const { toast } = callbacks;
+
+  autoUpdater.on('update-available', () => {
+    toast?.('Atualização encontrada, baixando agora...');
+    info('AutoUpdater encontrou nova versão', { metadata: { action: 'update-available' } });
+  });
+
+  autoUpdater.on('update-not-available', () => {
+    toast?.('Nenhuma atualização disponível no momento.');
+    info('AutoUpdater não encontrou novas versões', { metadata: { action: 'update-not-available' } });
+  });
+
   autoUpdater.on('error', (err) => {
     error('AutoUpdater falhou', { metadata: { error: err } });
+    toast?.('Erro ao buscar atualizações.');
   });
 
   autoUpdater.on('update-downloaded', () => {
     info('Atualização baixada e aguardando instalação', {
       metadata: { action: 'update-downloaded' }
     });
+    toast?.('Atualização baixada. Aplicando agora...');
     try {
       autoUpdater.quitAndInstall();
     } catch (err) {
       warn('Falha ao aplicar atualização automaticamente', {
         metadata: { error: err }
       });
+      toast?.('Não foi possível aplicar a atualização automaticamente.');
     }
   });
 }
 
+function checkForUpdates(autoUpdater, callbacks = {}) {
+  const { toast } = callbacks;
+  try {
+    toast?.('Buscando atualizações...');
+    autoUpdater.checkForUpdatesAndNotify();
+  } catch (err) {
+    warn('Falha ao iniciar busca de atualizações', {
+      metadata: { error: err }
+    });
+    toast?.('Erro ao iniciar busca de atualizações.');
+  }
+}
+
 module.exports = {
-  attachAutoUpdaterHandlers
+  attachAutoUpdaterHandlers,
+  checkForUpdates
 };
