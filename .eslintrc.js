434011ccc29ec86e8a624654ef838867aac87dd0
joaosn <jv.zyzz.legado@gmail.com>
2026-02-26 21:35:20 -0300

    Atualiza a versão do ECMAScript para 2020 e desabilita a regra de estilo de quebra de linha no ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,9 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
   },
   rules: {
+    'linebreak-style': 'off',
   },
 };
